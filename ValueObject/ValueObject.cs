b5f651a2d0925158e86d7a59a8e2e5754725de4c
Justin Hewlett <hewlett.jus@gmail.com>
2013-11-06 20:47:51 -0700

    initial commit.

---
+++ ValueObject/ValueObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObject
+{
+    //public class IgnoreAttribute : Attribute
+    //{
+    //}
+
+    public class ValueObject<T> : IEquatable<T> where T : class
+    {
+        public static bool operator ==(ValueObject<T> obj1, ValueObject<T> obj2)
+        {
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(ValueObject<T> obj1, ValueObject<T> obj2)
+        {
+            return !obj1.Equals(obj2);
+        }
+
+        public bool Equals(T obj)
+        {
+            return Equals(obj as object);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return GetProperties().All(p => PropertiesAreEqual(obj, p))
+                && GetFields().All(f => object.Equals(f.GetValue(this), f.GetValue(obj)));
+        }
+
+        private bool PropertiesAreEqual(object obj, PropertyInfo p)
+        {
+            //get test failing when obj is null
+            //if (obj.GetType() != typeof(ValueObject<T>)) return false;
+
+            var thisValue = p.GetValue(this, null);
+            var otherValue = p.GetValue(obj, null);
+
+            return object.Equals(thisValue, otherValue);
+        }
+
+        private IEnumerable<PropertyInfo> GetProperties()
+        {
+            return GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                //.Where(p => p.GetCustomAttribute(typeof(IgnoreAttribute)) != null);
+        }
+
+        private FieldInfo[] GetFields()
+        {
+            return GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked   //allow overflow
+            {
+                int hash = 17;
+                foreach (var prop in GetProperties())
+                {   
+                    var value = prop.GetValue(this, null);
+                    hash = HashValue(hash, value);
+                }
+
+                foreach(var field in GetFields())
+                {
+                    var value = field.GetValue(this);
+                    hash = HashValue(hash, value);
+                }
+
+                return hash;
+            }
+        }
+
+        private int HashValue(int seed, object value)
+        {
+            var currentHash = value != null
+                ? value.GetHashCode()
+                : 0;
+
+            return seed * 23 + currentHash;
+        }
+    }
+}
---
+++ ValueObjectTests/ValueObjectTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ValueObject;
+using System.Windows;
+using System.Drawing;
+
+namespace ValueObjectTests
+{
+    class TestValue : ValueObject<TestValue>
+    {
+        public TestValue() { }
+
+        public TestValue(int nonPublicValue)
+        {
+            ProtectedProperty = nonPublicValue;
+            PrivateProperty = nonPublicValue;
+            privateField = nonPublicValue;
+            protectedField = nonPublicValue;
+        }
+
+        public string Property1 { get; set; }
+        public int Property2 { get; set; }
+        public int Field;
+        protected int protectedField;
+        private int privateField;
+
+        protected int ProtectedProperty { get; set; }
+        private int PrivateProperty { get; set; }
+    }
+
+    [TestClass]
+    public class ValueObjectTests
+    {
+        [TestMethod]
+        public void Equals_NullIsConsideredEqual()
+        {
+            var value1 = new TestValue();
+            var value2 = new TestValue();
+
+            AssertEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void Equals_OnlyOneValueIsNull_DoesNotThrow_NotEqual()
+        {
+            var value1 = new TestValue();
+            var value2 = new TestValue { Property1 = "value" };
+
+            AssertNotEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void Equals_ComparesAllPropertiesAndFields_Equal()
+        {
+            var value1 = new TestValue { Property1 = "test", Property2 = 10, Field = 3 };
+            var value2 = new TestValue { Property1 = "test", Property2 = 10, Field = 3 };
+
+            AssertEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void Equals_ComparesAllPropertiesAndFields_PropertyDifferent_NotEqual()
+        {
+            var value1 = new TestValue { Property1 = "test", Property2 = 10 };
+            var value2 = new TestValue { Property1 = "Test", Property2 = 10 };
+
+            AssertNotEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void Equals_ComparesAllPropertiesAndFields_FieldDifferent_NotEqual()
+        {
+            var value1 = new TestValue { Property1 = "test", Property2 = 10, Field = 8 };
+            var value2 = new TestValue { Property1 = "test", Property2 = 10, Field = 9 };
+
+            AssertNotEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void Equals_IgnoresPrivatePropertiesAndFields()
+        {
+            var value1 = new TestValue(5);
+            var value2 = new TestValue(8);
+
+            AssertEqual(value1, value2);
+        }
+
+        [TestMethod]
+        public void Equals_ComparingWithWrongType_ReturnsFalse()
+        {
+            var value = new TestValue { Property1 = "string" };
+
+            Assert.AreNotEqual(value, null);
+        }
+
+        [TestMethod]
+        public void ImplementsIEquatable()
+        {
+            var value = new TestValue();
+
+            Assert.IsInstanceOfType(value, typeof(IEquatable<TestValue>));
+        }
+
+        [TestMethod]
+        public void GetHashCode_AlwaysEqualForEqualObjects()
+        {
+            var value1 = new TestValue { Property1 = "string", Property2 = 4 };
+            var value2 = new TestValue { Property1 = "string", Property2 = 4 };
+
+            Assert.AreEqual(value1.GetHashCode(), value2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_NotEqualForDistinctObjects_1()
+        {
+            var value1 = new TestValue { Property1 = "string", Property2 = 4 };
+            var value2 = new TestValue { Property1 = "string", Property2 = 5 };
+
+            Assert.AreNotEqual(value1.GetHashCode(), value2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_NotEqualForDistinctObjects_2()
+        {
+            var value1 = new TestValue { Property1 = "string", Property2 = 4 };
+            var value2 = new TestValue { Property1 = "String", Property2 = 4 };
+
+            Assert.AreNotEqual(value1.GetHashCode(), value2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_HandlesNull()
+        {
+            var value1 = new TestValue { Property2 = 2 };
+            var value2 = new TestValue { Property2 = 5 };
+
+            Assert.AreNotEqual(value1.GetHashCode(), value2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_ConsidersPublicFields()
+        {
+            var value1 = new TestValue { Property2 = 2 };
+            var value2 = new TestValue { Property2 = 2, Field = 4 };
+
+            Assert.AreNotEqual(value1.GetHashCode(), value2.GetHashCode());
+        }
+
+        private void AssertEqual(TestValue value1, TestValue value2)
+        {
+            Assert.AreEqual(value1, value2);
+            Assert.IsTrue(value1 == value2);
+            Assert.IsFalse(value1 != value2);
+            Assert.IsTrue(value1.Equals(value2));
+        }
+
+        private void AssertNotEqual(TestValue value1, TestValue value2)
+        {
+            Assert.AreNotEqual(value1, value2);
+            Assert.IsTrue(value1 != value2);
+            Assert.IsFalse(value1 == value2);
+            Assert.IsFalse(value1.Equals(value2));
+        }
+    }
+}
